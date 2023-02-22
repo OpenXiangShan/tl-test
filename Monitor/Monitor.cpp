743d3ff47a1f2ff17d2175ece983b4b360437b93
Archer613 <2846509110@qq.com>
2023-02-22 18:15:47 +0800

    Complete the Monitor

--- Monitor/Monitor.cpp
+++ Monitor/Monitor.cpp
@@ -10,17 +10,334 @@ namespace tl_monitor{
     return this->info;
   }
 
-  void Monitor::print_info(){
-    std::string type_string;
-    switch(this->bus_type){
-      case TILE_BUS_TYPE: type_string = "TILE" + std::to_string(this->id);break;
-      case L3_BUS_TYPE: type_string = "L3" + std::to_string(this->id);break;
-      case DMA_BUS_TYPE: type_string = "DMA";break;
-      default: type_string = "Unkown";
-    }
 
-    std::string print_info = type_string + " Monitor is speaking!";
-    printf("%s\n", print_info.c_str());
+  void Monitor::fire_ULA(){
+    TLInfo a = *this->info;
+    if (*a.a_valid&&*a.a_ready)
+    {
+      info_chnl.channel = "A";
+      switch(*a.a_opcode){
+        case PutFullData:
+          info_chnl.op = "PutFullData";
+          info_chnl.param = "-";
+          break;
+        case PutPartialData:
+          info_chnl.op = "PutPartialData ";
+          info_chnl.param = "-";
+          break;
+        case Get:
+          info_chnl.op = "Get";
+          info_chnl.param = "-";
+          break;
+        default:info_chnl.op = "error";
+      }
+
+      info_chnl.addr = std::to_string(*a.a_address);
+      info_chnl.source = std::to_string(*a.a_source);
+      info_chnl.clear_data();
+      for (size_t i = 0; i < BEATSIZE; i++)
+      {
+        info_chnl.data = info_chnl.data + std::to_string(a.a_data[i]);
+      }
+      //user
+      info_chnl.alias = std::to_string(*a.a_user_alias);
+      info_chnl.prefercache = std::to_string(*a.a_user_preferCache);
+      info_chnl.needHint = std::to_string(*a.a_user_needHint);
+      //print
+      std::string print_info = info_base.add() + info_chnl.ula_add();
+      printf("%s\n", print_info.c_str());
+    }
+  }
+
+
+  void Monitor::fire_ULD(){
+    TLInfo d = *this->info;
+    if (*d.d_valid&&*d.d_ready)
+    {
+      info_chnl.channel = "D";
+      switch(*d.d_opcode){
+        case AccessAck:
+          info_chnl.op = "AccessAck";
+          info_chnl.param = "-";
+          break;
+        case AccessAckData:
+          info_chnl.op = "AccessAckData";
+          info_chnl.param = "-";
+        default:info_chnl.op = "error";
+      }
+
+      info_chnl.addr = "-";
+      info_chnl.source = std::to_string(*d.d_source);
+      info_chnl.sink = std::to_string(*d.d_sink);
+      info_chnl.clear_data();
+      for (size_t i = 0; i < BEATSIZE; i++)
+      {
+        info_chnl.data = info_chnl.data + std::to_string(d.d_data[i]);
+      }
+      //print
+      std::string print_info = info_base.add() + info_chnl.uld_add();
+      printf("%s\n", print_info.c_str());
+    }
+  }
+
+  void Monitor::fire_ULAgent(){
+    fire_ULA();
+    fire_ULD();
+  }
+
+
+  void Monitor::fire_CA(){
+    TLInfo a = *this->info;
+    if (*a.a_valid&&*a.a_ready)
+    {
+      info_chnl.channel = "A";
+      switch(*a.a_opcode){
+        //TL-C
+        case AcquireBlock:
+          info_chnl.op = "AcquireBlock";
+          switch (*a.a_param)
+          {
+          case NtoT:info_chnl.param = "NtoT";break;
+          case NtoB:info_chnl.param = "NtoB";break;
+          case BtoT:info_chnl.param = "BtoT";break;
+          default:info_chnl.param = "error";break;
+          }
+          break;
+        case AcquirePerm:
+          info_chnl.op = "AcquirePerm";
+          if(*a.a_param == NtoT)
+            info_chnl.param = "NtoT";
+          else
+            info_chnl.param = "error";
+          break;
+        //TL-UH
+        case Hint:
+          info_chnl.op = "Hint";
+          info_chnl.param = "-";
+        //TL-UL
+        case PutFullData:
+          info_chnl.op = "PutFullData";
+          info_chnl.param = "-";
+          break;
+        case PutPartialData:
+          info_chnl.op = "PutPartialData";
+          info_chnl.param = "-";
+          break;
+        case Get:
+          info_chnl.op = "Get";
+          info_chnl.param = "-";
+          break;
+        default:info_chnl.op = "error";break;
+      }
+
+      info_chnl.addr = std::to_string(*a.a_address);
+      info_chnl.source = std::to_string(*a.a_source);
+      info_chnl.clear_data();
+      for (size_t i = 0; i < BEATSIZE; i++)
+      {
+        info_chnl.data = info_chnl.data + std::to_string(a.a_data[i]);
+      }
+      //user
+      info_chnl.alias = std::to_string(*a.a_user_alias);
+      info_chnl.prefercache = std::to_string(*a.a_user_preferCache);
+      info_chnl.needHint = std::to_string(*a.a_user_needHint);
+      //print
+      std::string print_info = info_base.add() + info_chnl.ca_add();
+      printf("%s\n", print_info.c_str());
+    }
+  }
+
+  void Monitor::fire_CB(){
+    TLInfo b = *this->info;
+    if (*b.b_valid&&*b.b_ready)
+    {
+      info_chnl.channel = "B";
+      switch(*b.b_opcode){
+        case Probe:
+          info_chnl.op = "Probe";
+          switch (*b.b_param)
+          {
+          case toT:info_chnl.param = "toT";break;
+          case toB:info_chnl.param = "toB";break;
+          case toN:info_chnl.param = "toN";break;
+          default:info_chnl.param = "error";break;
+          }
+          break;
+        default:info_chnl.op = "error";
+      }
+      
+      info_chnl.addr = std::to_string(*b.b_address);
+      info_chnl.source = std::to_string(*b.d_source);
+      info_chnl.clear_data();
+      info_chnl.data = "-";
+      //uesr
+      info_chnl.alias = std::to_string(*b.b_alias);
+      info_chnl.needdata = std::to_string(*b.b_needdata);
+      //print
+      std::string print_info = info_base.add() + info_chnl.cb_add();
+      printf("%s\n", print_info.c_str());
+    }
+  }
+
+  void Monitor::fire_CC(){
+    TLInfo c = *this->info;
+    if (*c.c_valid&&*c.c_ready)
+    {
+      info_chnl.channel = "C";
+      switch(*c.c_opcode){
+        case ProbeAck:
+          info_chnl.op = "ProbeAck";
+          switch (*c.c_param)
+          {
+          case TtoB:info_chnl.param = "TtoB";break;
+          case TtoN:info_chnl.param = "TtoN";break;
+          case BtoN:info_chnl.param = "BtoN";break;
+          case TtoT:info_chnl.param = "TtoTwarning";break;
+          case BtoB:info_chnl.param = "BtoBwarning";break;
+          case NtoN:info_chnl.param = "NtoNwarning";break;
+          default:info_chnl.param = "error";break;
+          }
+          break;
+        case ProbeAckData:
+          info_chnl.op = "ProbeAckData";
+          switch (*c.c_param)
+          {
+          case TtoB:info_chnl.param = "TtoB";break;
+          case TtoN:info_chnl.param = "TtoN";break;
+          case BtoN:info_chnl.param = "BtoN";break;
+          case TtoT:info_chnl.param = "TtoTwarning";break;
+          case BtoB:info_chnl.param = "BtoBwarning";break;
+          case NtoN:info_chnl.param = "NtoNwarning";break;
+          default:info_chnl.param = "error";break;
+          }
+          break;
+        case Release:
+          info_chnl.op = "Release";
+          switch (*c.c_param)
+          {
+          case TtoB:info_chnl.param = "TtoB";break;
+          case TtoN:info_chnl.param = "TtoN";break;
+          case BtoN:info_chnl.param = "BtoN";break;
+          default:info_chnl.param = "error";break;
+          }
+          break;
+        case ReleaseData:
+          info_chnl.op = "ReleaseData";
+          switch (*c.c_param)
+          {
+          case TtoB:info_chnl.param = "TtoB";break;
+          case TtoN:info_chnl.param = "TtoN";break;
+          case BtoN:info_chnl.param = "BtoN";break;
+          default:info_chnl.param = "error";break;
+          }
+          break;
+        default:info_chnl.op = "error";
+      }
+      info_chnl.addr = std::to_string(*c.c_address);
+      info_chnl.source = std::to_string(*c.d_source);
+      info_chnl.clear_data();
+      info_chnl.data = "-";
+      //uesr
+      info_chnl.blockisdirty = std::to_string(*c.c_echo_blockisdirty);
+      //print
+      std::string print_info = info_base.add() + info_chnl.cc_add();
+      printf("%s\n", print_info.c_str());
+    }
+  }
+
+  void Monitor::fire_CD(){
+    TLInfo d = *this->info;
+    if (*d.d_valid&&*d.d_ready)
+    {
+      info_chnl.channel = "D";
+      switch(*d.d_opcode){
+        case AccessAck:
+          info_chnl.op = "AccessAck";
+          info_chnl.param = "-";
+          break;
+        case AccessAckData:
+          info_chnl.op = "AccessAckData";
+          info_chnl.param = "-";
+        case Grant:
+          info_chnl.op = "Grant";
+          switch (*d.d_param)
+          {
+          case toT:info_chnl.param = "toT";break;
+          case toB:info_chnl.param = "toB";break;
+          case toN:info_chnl.param = "toN";break;
+          default:info_chnl.param = "error";break;
+          }
+          break;
+        case GrantData:
+          info_chnl.op = "GrantData";
+          switch (*d.d_param)
+          {
+          case toT:info_chnl.param = "toT";break;
+          case toB:info_chnl.param = "toB";break;
+          case toN:info_chnl.param = "toN";break;
+          default:info_chnl.param = "error";break;
+          }
+          break;
+        default:info_chnl.op = "error";
+      }
+
+      info_chnl.addr = "-";
+      info_chnl.source = std::to_string(*d.d_source);
+      info_chnl.sink = std::to_string(*d.d_sink);
+      info_chnl.clear_data();
+      for (size_t i = 0; i < BEATSIZE; i++)
+      {
+        info_chnl.data = info_chnl.data + std::to_string(d.d_data[i]);
+      }
+      //user
+      info_chnl.blockisdirty = std::to_string(*d.d_echo_blockisdirty);
+      //print
+      std::string print_info = info_base.add() + info_chnl.cd_add();
+      printf("%s\n", print_info.c_str());
+    }
+  }
+
+  void Monitor::fire_CE(){
+    TLInfo e = *this->info;
+    if(*e.e_valid && *e.e_ready){
+      info_chnl.channel = "E";
+      info_chnl.op = "GrantAck";
+      info_chnl.param = "-";
+      info_chnl.sink = std::to_string(*e.e_sink);
+      //print
+      std::string print_info = info_base.add() + info_chnl.ce_add();
+      printf("%s\n", print_info.c_str());
+    }
+  }
+
+  void Monitor::fire_CAgent(){
+    fire_CA();
+    fire_CB();
+    fire_CC();
+    fire_CD();
+    fire_CE();
+  }
+
+
+  //core 0 d$: [100] [A] [AcquireData] [NtoT] addr: 400 source: 0 sink: 0 data: 0
+  //alias: 0 preferCache: 0 needdata: 0 needHint: 0 blockisdirty: 0
+  void Monitor::print_info(){
+    info_base.cycle = std::to_string(*this->cycle);
+    switch(this->bus_type){
+      case TILE_BUS_TYPE: 
+        info_base.module = "core "+ std::to_string(this->id) + " L2: ";
+        fire_CAgent();
+        break;
+      case L3_BUS_TYPE: 
+        info_base.module = "L3 - bank" + std::to_string(this->id) + " ";
+        fire_CAgent();
+        break;
+      case DMA_BUS_TYPE: 
+        info_base.module = "uncache - DMA: ";
+        fire_ULAgent(); 
+        break;
+      default:printf("Unkown\n");break;
+    }
   }
 
 }
--- Monitor/Monitor.h
+++ Monitor/Monitor.h
@@ -4,6 +4,71 @@
 #include "../Utils/Common.h"
 
 namespace tl_monitor{
+
+//core 0 d$: [100] [A] [AcquireData NtoT] addr: 400 source: 0 sink: 0 data: 0
+//alias: 0 preferCache: 0 needdata: 0 needHint: 0 blockisdirty: 0
+using namespace std;
+class info_BaseEntry{
+  private:
+    string sum;
+  public:
+    string module;
+    string cycle;
+    string add(){return sum = module + "[" +cycle + "] ";}
+};
+
+class info_ChnlEntry{
+  private:
+    string sum;
+  public:
+    string channel;
+    string op;
+    string param;
+    string addr;
+    string source;
+    string sink;
+    string data;
+    //uesr
+    string alias;
+    string prefercache;
+    string needHint;
+    string needdata;
+    string blockisdirty;
+    //UL-channel
+    string ula_add(){return sum = "["+channel +"] " + "[" + op + " " + param + "] " 
+                + "addr: " + addr + " source: " + source
+                + " preferCache: " + prefercache//user
+                + " data: " + data;}//data
+    string uld_add(){return sum = "["+channel +"] " + "[" + op + " " + param + "] " 
+                + "addr: " + addr + " source: " + source + " sink: " + sink
+                + " data: " + data;}//data
+    
+    //C-channel
+    string ca_add(){return sum = "["+channel +"] " + "[" + op + " " + param + "] " 
+                + "addr: " + addr + " source: " + source
+                + " alias: " + alias + " preferCache: " + prefercache + " needHint: " + needHint//user
+                + " data: " + data;}//data
+
+    string cb_add(){return sum = "["+channel +"] " + "[" + op + " " + param + "] " 
+                + "addr: " + addr + " source: " + source
+                + " alias: " + alias + " needdata: " + needdata//user
+                + " data: " + data;}//data
+    string cc_add(){return sum = "["+channel +"] " + "[" + op + " " + param + "] " 
+                + "addr: " + addr + " source: " + source
+                + " blockisdirty: " + blockisdirty//user
+                + " data: " + data;}//data
+    string cd_add(){return sum = "["+channel +"] " + "[" + op + " " + param + "] " 
+                + "addr: " + addr + " source: " + source + " sink: " + sink
+                + " blockisdirty: " + blockisdirty//user
+                + " data: " + data;}//data
+     string ce_add(){return sum = "["+channel +"] " + "[" + op + " " + param + "] " 
+                + " sink: " + sink;};
+    
+    
+    void clear_data(){data = "";};
+};
+
+
 using namespace tl_interface;
 class Monitor{
   private:
@@ -11,10 +76,22 @@ class Monitor{
   uint64_t id;
   uint8_t bus_type;
   std::shared_ptr<TLInfo> info;
+  info_BaseEntry info_base;
+  info_ChnlEntry info_chnl;
   public:
   Monitor(uint64_t* c, uint64_t iid, uint8_t bt);
   std::shared_ptr<TLInfo> get_info();
   void print_info();
+  void fire_ULA();
+  void fire_ULD();
+  void fire_CA();
+  void fire_CB();
+  void fire_CC();
+  void fire_CD();
+  void fire_CE();
+
+  void fire_CAgent();
+  void fire_ULAgent();
 };
 }
 #endif //TLC_TEST_MONITOR_H
