f710f212ca6d53073b1a600788246f39e416b136
Kumonda221 <kumonda@kucro3.org>
2024-05-09 22:19:07 +0800

    TLGlobal: bump up to tl-test-new and better verbose control

--- Base/TLGlobal.hpp
+++ Base/TLGlobal.hpp
@@ -5,13 +5,14 @@
 
 
 #define TLTP_LOG_GLOBAL(str_proc) \
-    { std::cout << Gravity::StringAppender().Append("[tl-test-passive] ", __FILE__, ": ").str_proc.ToString(); }
+    { std::cout << Gravity::StringAppender().Append("[tl-test-new] ", __FILE__, ": ").str_proc.ToString(); }
     
 
 struct TLGlobalConfiguration {
 
     bool                    verbose;
     bool                    verbose_detailed_dpi;
+    bool                    verbose_data_full;
 };
 
 struct TLGlobalContext {
