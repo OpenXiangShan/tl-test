9ef639b81dd869f58d4c663cd035a87f65f4b39d
Kumonda221 <kumonda@kucro3.org>
2024-05-09 22:17:20 +0800

    ChiselDB: added plugin to officially support ChiselDB

---
+++ Plugins/ChiselDB.cpp
@@ -0,0 +1,62 @@
+#include "ChiselDB.hpp"
+#include "../../coupledL2/build/chisel_db.cpp"
+
+#include "../Utils/Common.h"
+
+
+//
+void ChiselDB::InitDB()
+{
+    init_db(true, false, "");
+}
+
+//
+void ChiselDB::SaveDB(const char* file)
+{
+    save_db(file);
+}
+
+
+// Implementation of: class PluginInstance
+namespace ChiselDB {
+
+    PluginInstance::PluginInstance() noexcept
+        : Plugin    ("chiseldb")
+    { }
+
+    std::string PluginInstance::GetDisplayName() const noexcept
+    {
+        return "ChiselDB";
+    }
+
+    std::string PluginInstance::GetDescription() const noexcept
+    {
+        return "ChiselDB Compatibility Plugin for TL-Test New";
+    }
+
+    std::string PluginInstance::GetVersion() const noexcept
+    {
+        return "Kunming Lake";
+    }
+
+    void PluginInstance::OnEnable()
+    {
+        LogInfo("ChiselDB", Append("Enabled").EndLine());
+
+        InitDB();
+
+        LogInfo("ChiselDB", Append("DB Initialized").EndLine());
+    }
+
+    void PluginInstance::OnDisable()
+    {
+        const char* file = "logs/chiseldb.db";
+
+        LogInfo("ChiselDB", Append("Saving DB to: ", file).EndLine());
+
+        SaveDB(file);
+
+        LogInfo("ChiselDB", Append("Saved DB").EndLine());
+        LogInfo("ChiselDB", Append("Disabled").EndLine());
+    }
+}
---
+++ Plugins/ChiselDB.hpp
@@ -0,0 +1,37 @@
+#pragma once
+//
+//
+//
+
+#ifndef TLC_TEST_PLUGINS_CHISEL_DB
+#define TLC_TEST_PLUGINS_CHISEL_DB
+
+#include "../../coupledL2/build/chisel_db.h"
+
+#include "PluginManager.hpp"
+
+
+namespace ChiselDB {
+
+    //
+    void InitDB();
+    void SaveDB(const char* file);
+
+    //
+    class PluginInstance : public Plugin {
+    public:
+        PluginInstance() noexcept;
+
+    public:
+        std::string     GetDisplayName() const noexcept override;
+        std::string     GetDescription() const noexcept override;
+        std::string     GetVersion() const noexcept override;
+
+        void            OnEnable() override;
+        void            OnDisable() override;
+    };
+    
+}
+
+
+#endif // TLC_TEST_PLUGINS_CHISEL_DB
